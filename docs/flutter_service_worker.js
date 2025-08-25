d664c903c9d90751379c96354c99b4d72416cc27
Som <somsubhra.pandit@gmail.com>
2025-08-26 00:20:13 +0530

    new build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -112,7 +112,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7b6804f106d04e557ab5aeed6d37c1a9
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "560c82b3e6364c978b591f3ad0ca1893",
+"flutter_bootstrap.js": "6ea04cd92ff1756684b29316247331f6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
